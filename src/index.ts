9bacf56b28a5d3e14a4b15222dd668a4d56950e3
Gevorg <gevorgdavtyan99@mail.ru>
2024-08-29 17:10:30 +0400

    add log

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+console.log("process", process);
 // Middleware to parse JSON bodies
 app.use(express.json());
 
